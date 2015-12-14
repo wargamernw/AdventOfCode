bf87eb1fb266a65ba548c2e5100cafcebda96e0d
John Anderson <ai_guy@msn.com>
2015-12-14 15:36:18 -0800

    Day 7

---
+++ AdventOfCode/Day7.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day7
+    {
+        enum LightMode
+        {
+            ON,
+            OFF,
+            TOGGLE
+        };
+
+        const int maxX = 1000;
+        const int maxY = 1000;
+
+        static BitArray[] lights = new BitArray[maxY];
+        static int[,] brightLights = new int[maxX, maxY];
+
+        public static int ProcessDay7(out long count2)
+        {
+            int count = 0;
+            count2 = 0;
+
+            string filePath = "..\\..\\Data\\Day7.txt";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
+
--- AdventOfCode/MainWindow.xaml.cs
+++ AdventOfCode/MainWindow.xaml.cs
@@ -28,7 +28,19 @@ namespace AdventOfCode
         public string Day2Result2 { get; set; }
 
         public string Day3Result { get; set; }
-        public string Day3Result3 { get; set; }
+        public string Day3Result2 { get; set; }
+
+        public string Day4Result { get; set; }
+        public string Day4Result2 { get; set; }
+
+        public string Day5Result { get; set; }
+        public string Day5Result2 { get; set; }
+
+        public string Day6Result { get; set; }
+        public string Day6Result2 { get; set; }
+
+        public string Day7Result { get; set; }
+        public string Day7Result2 { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -49,21 +61,73 @@ namespace AdventOfCode
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Button1_Click(sender, e);
+            Button2_Click(sender, e);
+            Button3_Click(sender, e);
+            Button4_Click(sender, e);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
             int optional;
             Day1Result = Day1.ProcessDay1(out optional).ToString();
             OnPropertyChanged("Day1Result");
             Day1Result2 = optional.ToString();
             OnPropertyChanged("Day1Result2");
+        }
 
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            int optional;
             Day2Result = Day2.ProcessDay2(out optional).ToString();
             OnPropertyChanged("Day2Result");
             Day2Result2 = optional.ToString();
             OnPropertyChanged("Day2Result2");
+        }
 
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            int optional;
             Day3Result = Day3.ProcessDay3(out optional).ToString();
             OnPropertyChanged("Day3Result");
-            Day3Result3 = optional.ToString();
-            OnPropertyChanged("Day3Result3");
+            Day3Result2 = optional.ToString();
+            OnPropertyChanged("Day3Result2");
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            long optional;
+            Day4Result = Day4.ProcessDay4(out optional).ToString();
+            OnPropertyChanged("Day4Result");
+            Day4Result2 = optional.ToString();
+            OnPropertyChanged("Day4Result2");
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            int optional;
+            Day5Result = Day5.ProcessDay5(out optional).ToString();
+            OnPropertyChanged("Day5Result");
+            Day5Result2 = optional.ToString();
+            OnPropertyChanged("Day5Result2");
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            long optional;
+            Day6Result = Day6.ProcessDay6(out optional).ToString();
+            OnPropertyChanged("Day6Result");
+            Day6Result2 = optional.ToString();
+            OnPropertyChanged("Day6Result2");
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            long optional;
+            Day7Result = Day7.ProcessDay7(out optional).ToString();
+            OnPropertyChanged("Day7Result");
+            Day7Result2 = optional.ToString();
+            OnPropertyChanged("Day7Result2");
         }
     }
 }
